e8438bf1cb2ba2d400f00ac7007ca9dcad58a5af
ZeynepTikir <zeyneptikir@marun.edu.tr>
2025-10-16 11:51:04 +0300

    5. ödev eklendi

---
+++ odev5/ödev 5/ödev 5/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ödev_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.btnBitir = new System.Windows.Forms.Button();
+            this.panelOyun = new System.Windows.Forms.Panel();
+            this.listBoxKayit = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSure = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Location = new System.Drawing.Point(29, 169);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(75, 50);
+            this.btnBasla.TabIndex = 0;
+            this.btnBasla.Text = "OYUNA BAŞLA";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // btnBitir
+            // 
+            this.btnBitir.Location = new System.Drawing.Point(703, 169);
+            this.btnBitir.Name = "btnBitir";
+            this.btnBitir.Size = new System.Drawing.Size(75, 50);
+            this.btnBitir.TabIndex = 0;
+            this.btnBitir.Text = "OYUNU BİTİR";
+            this.btnBitir.UseVisualStyleBackColor = true;
+            this.btnBitir.Click += new System.EventHandler(this.btnBitir_Click);
+            // 
+            // panelOyun
+            // 
+            this.panelOyun.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panelOyun.Location = new System.Drawing.Point(110, 12);
+            this.panelOyun.Name = "panelOyun";
+            this.panelOyun.Size = new System.Drawing.Size(461, 354);
+            this.panelOyun.TabIndex = 2;
+            // 
+            // listBoxKayit
+            // 
+            this.listBoxKayit.FormattingEnabled = true;
+            this.listBoxKayit.Location = new System.Drawing.Point(577, 11);
+            this.listBoxKayit.Name = "listBoxKayit";
+            this.listBoxKayit.Size = new System.Drawing.Size(120, 355);
+            this.listBoxKayit.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(703, 248);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "SÜRE";
+            // 
+            // lblSure
+            // 
+            this.lblSure.AutoSize = true;
+            this.lblSure.Location = new System.Drawing.Point(703, 275);
+            this.lblSure.Name = "lblSure";
+            this.lblSure.Size = new System.Drawing.Size(37, 13);
+            this.lblSure.TabIndex = 4;
+            this.lblSure.Text = "60 SN";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 381);
+            this.Controls.Add(this.lblSure);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxKayit);
+            this.Controls.Add(this.panelOyun);
+            this.Controls.Add(this.btnBitir);
+            this.Controls.Add(this.btnBasla);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Button btnBitir;
+        private System.Windows.Forms.Panel panelOyun;
+        private System.Windows.Forms.ListBox listBoxKayit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSure;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ odev5/ödev 5/ödev 5/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ödev_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        List<int> butonSayilari = new List<int>();
+        List<Button> tiklananButonlar = new List<Button>();
+        int kalanSure = 60;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+        }
+
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            panelOyun.Controls.Clear();
+            butonSayilari.Clear();
+            tiklananButonlar.Clear();
+            listBoxKayit.Items.Clear();
+            kalanSure = 60;
+            lblSure.Text = "60 SN";
+
+            timer1.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int s = rnd.Next(1, 101);
+                butonSayilari.Add(s);
+
+                Button btn = new Button();
+                btn.Text = s.ToString();
+                btn.Width = 60;
+                btn.Height = 40;
+                btn.Tag = s;
+                btn.BackColor = Color.White;
+                btn.Location = new Point(
+                    rnd.Next(10, panelOyun.Width - 70),
+                    rnd.Next(10, panelOyun.Height - 50)
+                );
+
+                btn.Click += SayiButon_Click;
+                panelOyun.Controls.Add(btn);
+            }
+        }
+
+        private void SayiButon_Click(object sender, EventArgs e)
+        {
+            Button tiklanan = (Button)sender;
+            tiklanan.Visible = false;
+            tiklananButonlar.Add(tiklanan);
+          
+            listBoxKayit.Items.Add((int)tiklanan.Tag);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            kalanSure--;
+            lblSure.Text = $"{kalanSure} SN";
+
+            if (kalanSure <= 0)
+            {
+                timer1.Stop();
+                KontrolEt(sureDoldu: true);
+            }
+
+            foreach (var btn in panelOyun.Controls.OfType<Button>())
+            {
+                if (!btn.Visible) continue;
+
+                btn.Location = new Point(
+                    rnd.Next(0, panelOyun.Width - btn.Width),
+                    rnd.Next(0, panelOyun.Height - btn.Height)
+                );
+            }
+        }
+
+        private void btnBitir_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            KontrolEt();
+        }
+
+        private void KontrolEt(bool sureDoldu = false)
+        {
+            List<int> dogruSiraliCiftler = new List<int>();
+            for (int i = 0; i < butonSayilari.Count; i++)
+            {
+                int sayi = butonSayilari[i];
+                if (sayi % 2 == 0)
+                    dogruSiraliCiftler.Add(sayi);
+            }
+
+            for (int i = 0; i < dogruSiraliCiftler.Count - 1; i++)
+            {
+                for (int j = 0; j < dogruSiraliCiftler.Count - i - 1; j++)
+                {
+                    if (dogruSiraliCiftler[j] > dogruSiraliCiftler[j + 1])
+                    {
+                        int temp = dogruSiraliCiftler[j];
+                        dogruSiraliCiftler[j] = dogruSiraliCiftler[j + 1];
+                        dogruSiraliCiftler[j + 1] = temp;
+                    }
+                }
+            }
+
+            List<int> tiklananCiftler = new List<int>();
+            for (int i = 0; i < tiklananButonlar.Count; i++)
+            {
+                int sayi = (int)tiklananButonlar[i].Tag;
+                if (sayi % 2 == 0)
+                    tiklananCiftler.Add(sayi);
+            }
+
+            // 3️⃣ Doğru mu kontrol et (sıralı mı)
+            bool dogruMu = true;
+            if (dogruSiraliCiftler.Count != tiklananCiftler.Count)
+            {
+                dogruMu = false;
+            }
+            else
+            {
+                for (int i = 0; i < dogruSiraliCiftler.Count; i++)
+                {
+                    if (dogruSiraliCiftler[i] != tiklananCiftler[i])
+                    {
+                        dogruMu = false;
+                        break;
+                    }
+                }
+            }
+
+            if (sureDoldu)
+            {
+                MessageBox.Show("Süre doldu! Doğru tamamlayamadınız.", "Oyun Bitti");
+            }
+            else if (dogruMu)
+            {
+                MessageBox.Show($"Doğru tamamladınız! Kalan süre: {kalanSure} saniye", "Tebrikler!");
+            }
+            else
+            {
+                MessageBox.Show($"Yanlış tamamladınız! Kalan süre: {kalanSure} saniye", "Oyun Bitti");
+            }
+        }
+    }
+}
